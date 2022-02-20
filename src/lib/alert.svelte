aa00e1e3b04cd76d56e26491350e1545eea295c3
Vishal Mahajan <vishal423@gmail.com>
2022-02-20 20:00:03 +0530

    Rename type to contextualColor as prop name

--- src/lib/alert.svelte
+++ src/lib/alert.svelte
@@ -1,40 +1,42 @@
 <script>
-	export let testId = 'alert'
-	export let show = false
-	export let type = 'success'
-	export let closeable = true
+	export let testId = 'alert';
+	export let show = false;
+	export let contextualColor = 'success';
+	export let closeable = true;
 
 	function closeAlert() {
-		show = false
+		show = false;
 	}
 </script>
 
 <div
-	class:hidden="{!show}"
-	class:bg-green-200="{type === 'success'}"
-	class:dark:bg-green-200="{type === 'success'}"
-	class:text-green-900="{type === 'success'}"
-	class:dark:text-green-900="{type === 'success'}"
-	class:bg-red-200="{type === 'danger'}"
-	class:dark:bg-red-200="{type === 'danger'}"
-	class:text-red-900="{type === 'danger'}"
-	class:dark:text-red-900="{type === 'danger'}"
-	class:bg-yellow-100="{type === 'warning'}"
-	class:dark:bg-yellow-200="{type === 'warning'}"
-	class:text-yellow-900="{type === 'warning'}"
-	class:dark:text-yellow-900="{type === 'warning'}"
+	role="alert"
+	class:hidden={!show}
+	class:bg-green-200={contextualColor === 'success'}
+	class:dark:bg-green-200={contextualColor === 'success'}
+	class:text-green-900={contextualColor === 'success'}
+	class:dark:text-green-900={contextualColor === 'success'}
+	class:bg-red-200={contextualColor === 'danger'}
+	class:dark:bg-red-200={contextualColor === 'danger'}
+	class:text-red-900={contextualColor === 'danger'}
+	class:dark:text-red-900={contextualColor === 'danger'}
+	class:bg-yellow-100={contextualColor === 'warning'}
+	class:dark:bg-yellow-200={contextualColor === 'warning'}
+	class:text-yellow-900={contextualColor === 'warning'}
+	class:dark:text-yellow-900={contextualColor === 'warning'}
 	class="px-5 py-3 mt-4 flex justify-between transition-colors duration-150 rounded text-sm"
 >
-	<div data-test="{testId}-{type}" {...$$restProps}>
+	<div data-test="{testId}-{contextualColor}" {...$$restProps}>
 		<slot />
 	</div>
 	{#if closeable}
 		<button
-			class:hover:text-red-700="{type === 'danger'}"
-			class:dark:hover:text-red-500="{type === 'danger'}"
+			aria-label="Close"
+			class:hover:text-red-700={contextualColor === 'danger'}
+			class:dark:hover:text-red-500={contextualColor === 'danger'}
 			class="self-start text-xl leading-4 bg-transparent border-0
 				focus:outline-none"
-			on:click|preventDefault="{closeAlert}">&times</button
+			on:click|preventDefault={closeAlert}><span aria-hidden="true">&times</span></button
 		>
 	{/if}
 </div>
