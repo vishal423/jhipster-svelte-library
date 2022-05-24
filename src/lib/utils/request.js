c4e7157cb5b45e59bfa1470cb007c0e232becb1b
Vishal Mahajan <vishal423@gmail.com>
2022-05-25 00:31:47 +0530

    Improve request utility to make session and jwt server calls

--- src/lib/utils/request.js
+++ src/lib/utils/request.js
@@ -1,6 +1,64 @@
 import notificationService from '../notification/notification-service'
 
-export function handleResponse(response, notify) {
+export function request(
+	url,
+	method = 'GET',
+	body = undefined,
+	headers = {},
+	notify = true
+) {
+	const xhrHeaders = { ...headers, 'X-Requested-With': 'XMLHttpRequest' }
+	return fetch(url, prepareRequest(method, body, xhrHeaders))
+		.then(response => handleResponse(response, notify))
+		.catch(err => {
+			return Promise.reject(err)
+		})
+}
+
+function prepareRequest(method, body, headers) {
+	let requestHeaders = { ...headers }
+	if (['POST', 'PUT', 'DELETE'].includes(method)) {
+		const csrfCookieValue = getCookieValue('XSRF-TOKEN')
+		if (csrfCookieValue) {
+			requestHeaders = {
+				...requestHeaders,
+				'X-XSRF-TOKEN': csrfCookieValue,
+			}
+		}
+	}
+	const authToken =
+		localStorage.getItem('authToken') || sessionStorage.getItem('authToken')
+	if (authToken) {
+		requestHeaders = {
+			...requestHeaders,
+			Authorization: `Bearer ${authToken}`,
+		}
+	}
+
+	return {
+		method,
+		body,
+		credentials: 'include',
+		headers: requestHeaders,
+	}
+}
+
+function getCookieValue(cookieName) {
+	if (!document.cookie) {
+		return undefined
+	}
+
+	const values = document.cookie
+		.split(';')
+		.map(c => c.trim())
+		.filter(c => c.startsWith(cookieName + '='))
+		.map(c => c.split('=')[1])
+		.map(c => decodeURIComponent(c))
+
+	return values.length ? values[0] : undefined
+}
+
+function handleResponse(response, notify) {
 	if (notify) {
 		processAlertHeaders(response, response.ok)
 	}
