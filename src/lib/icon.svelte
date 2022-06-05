735d16ce061c12650a47108d79c9b98e82702c64
Vishal Mahajan <vishal423@gmail.com>
2022-06-05 15:01:33 +0530

    Handle transient null icon

--- src/lib/icon.svelte
+++ src/lib/icon.svelte
@@ -8,7 +8,7 @@
 	class="w-4 h-4 overflow-visible inline-block fill-current {classes}"
 	role="img"
 	xmlns="http://www.w3.org/2000/svg"
-	viewBox="0 0 {icon.icon[0]} {icon.icon[1]}"
+	viewBox="0 0 {icon ? icon.icon[0] : 0} {icon ? icon.icon[1] : 0}"
 >
-	<path fill="currentColor" d="{icon.icon[4]}"></path>
+	<path fill="currentColor" d="{icon ? icon.icon[4] : undefined}"></path>
 </svg>
