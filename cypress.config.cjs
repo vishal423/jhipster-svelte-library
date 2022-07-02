412ad9d180bc322a8db404db39e672ef07e41592
Vishal Mahajan <vishal423@gmail.com>
2022-07-02 13:08:04 +0530

    chore: Bump dependencies

---
+++ cypress.config.cjs
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+	e2e: {
+		baseUrl: 'http://localhost:3000',
+		video: false,
+		retries: 1,
+		experimentalSessionAndOrigin: true,
+		specPattern: 'cypress/integration/**/*.spec.js',
+		supportFile: 'cypress/support/index.js',
+	},
+})
