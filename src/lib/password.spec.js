f1fe8b292b3cfa946fd1c3097c5ad0a74608ebd1
Vishal Mahajan <vishal423@gmail.com>
2022-07-28 23:29:33 +0530

    feat: Add Password and ConfirmPassword components

---
+++ src/lib/password.spec.js
@@ -0,0 +1,198 @@
+import '@testing-library/jest-dom/extend-expect'
+import { jest } from '@jest/globals'
+import { render, fireEvent } from '@testing-library/svelte'
+import { screen } from '@testing-library/dom'
+
+import Password from './password.svelte'
+
+test('should display valid password control', async () => {
+	const { component } = render(Password, {
+		props: {
+			id: 'password',
+			value: 'someValue',
+		},
+	})
+
+	const mockHandler = jest.fn()
+	component.$on('validate', mockHandler)
+
+	const passwordInput = screen.getByLabelText(/password/i)
+
+	await fireEvent.input(passwordInput, { target: { value: 'newValue' } })
+
+	expect(passwordInput).toHaveValue('newValue')
+
+	expect(mockHandler).toHaveBeenCalled()
+	expect(mockHandler.mock.calls[0][0].detail).toStrictEqual({
+		valid: true,
+		message: undefined,
+	})
+})
+
+test('should validate password control default required constraint', async () => {
+	const { component } = render(Password, {
+		props: {
+			id: 'password',
+			value: 'someLongValue',
+		},
+	})
+
+	const mockHandler = jest.fn()
+	component.$on('validate', mockHandler)
+
+	const passwordInput = screen.getByLabelText(/password/i)
+
+	await fireEvent.input(passwordInput, { target: { value: '' } })
+
+	expect(passwordInput).toHaveValue('')
+
+	expect(mockHandler).toHaveBeenCalled()
+	expect(mockHandler.mock.calls[0][0].detail).toStrictEqual({
+		valid: false,
+		message: 'Password is mandatory',
+	})
+})
+
+test('should validate password control custom required constraint message', async () => {
+	const { component } = render(Password, {
+		props: {
+			id: 'password',
+			value: 'someLongValue',
+			validations: [
+				{ type: 'required', message: 'Password is mandatory field' },
+			],
+		},
+	})
+
+	const mockHandler = jest.fn()
+	component.$on('validate', mockHandler)
+
+	const passwordInput = screen.getByLabelText(/password/i)
+
+	await fireEvent.input(passwordInput, { target: { value: '' } })
+
+	expect(passwordInput).toHaveValue('')
+
+	expect(mockHandler).toHaveBeenCalled()
+	expect(mockHandler.mock.calls[0][0].detail).toStrictEqual({
+		valid: false,
+		message: 'Password is mandatory field',
+	})
+})
+
+test('should validate password control default minimum length constraint', async () => {
+	const { component } = render(Password, {
+		props: {
+			id: 'password',
+			value: 'someLongValue',
+		},
+	})
+
+	const mockHandler = jest.fn()
+	component.$on('validate', mockHandler)
+
+	const passwordInput = screen.getByLabelText(/password/i)
+
+	await fireEvent.input(passwordInput, { target: { value: 'ab' } })
+
+	expect(passwordInput).toHaveValue('ab')
+
+	expect(mockHandler).toHaveBeenCalled()
+	expect(mockHandler.mock.calls[0][0].detail).toStrictEqual({
+		valid: false,
+		message: 'Password is required to be at least 4 characters long',
+	})
+})
+
+test('should validate password control custom minimum length constraint message', async () => {
+	const { component } = render(Password, {
+		props: {
+			id: 'password',
+			value: 'someLongValue',
+			validations: [
+				{
+					type: 'minlength',
+					minlength: 2,
+					message:
+						'Password is required to be at least 2 characters long',
+				},
+			],
+		},
+	})
+
+	const mockHandler = jest.fn()
+	component.$on('validate', mockHandler)
+
+	const passwordInput = screen.getByLabelText(/password/i)
+
+	await fireEvent.input(passwordInput, { target: { value: 'a' } })
+
+	expect(passwordInput).toHaveValue('a')
+
+	expect(mockHandler).toHaveBeenCalled()
+	expect(mockHandler.mock.calls[0][0].detail).toStrictEqual({
+		valid: false,
+		message: 'Password is required to be at least 2 characters long',
+	})
+})
+
+test('should validate password control default maximum length constraint', async () => {
+	const { component } = render(Password, {
+		props: {
+			id: 'password',
+			value: 'someLongValue',
+		},
+	})
+
+	const mockHandler = jest.fn()
+	component.$on('validate', mockHandler)
+
+	const passwordInput = screen.getByLabelText(/password/i)
+
+	await fireEvent.input(passwordInput, {
+		target: {
+			value: 'loremipsumloremipsumloremipsumloremipsumloremipsumloremipsum',
+		},
+	})
+
+	expect(passwordInput).toHaveValue(
+		'loremipsumloremipsumloremipsumloremipsumloremipsumloremipsum'
+	)
+
+	expect(mockHandler).toHaveBeenCalled()
+	expect(mockHandler.mock.calls[0][0].detail).toStrictEqual({
+		valid: false,
+		message: 'Password cannot be longer than 50 characters',
+	})
+})
+
+test('should validate password control custom maximum length constraint message', async () => {
+	const { component } = render(Password, {
+		props: {
+			id: 'password',
+			value: 'val',
+			validations: [
+				{
+					type: 'maxlength',
+					maxlength: 5,
+					message: 'Password cannot be longer than 5 characters',
+				},
+			],
+		},
+	})
+
+	const mockHandler = jest.fn()
+	component.$on('validate', mockHandler)
+
+	const passwordInput = screen.getByLabelText(/password/i)
+
+	await fireEvent.input(passwordInput, { target: { value: 'loremipsum' } })
+
+	expect(passwordInput).toHaveValue('loremipsum')
+
+	expect(mockHandler).toHaveBeenCalled()
+	expect(mockHandler.mock.calls[0][0].detail).toStrictEqual({
+		valid: false,
+		message: 'Password cannot be longer than 5 characters',
+	})
+})
---
+++ src/lib/password.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle'
+
+	import InputControl from './input-control.svelte'
+	import Icon from './icon.svelte'
+
+	export let name = 'password'
+	export let label = 'Password'
+	export let value = ''
+	export let validations = [
+		{ type: 'required', message: 'Password is mandatory' },
+		{
+			type: 'minlength',
+			minlength: 4,
+			message: 'Password is required to be at least 4 characters long',
+		},
+		{
+			type: 'maxlength',
+			maxlength: 50,
+			message: 'Password cannot be longer than 50 characters',
+		},
+	]
+</script>
+
+<InputControl
+	type="password"
+	label="{label}"
+	value="{value}"
+	name="{name}"
+	validations="{validations}"
+	on:input
+	on:validate
+	let:message
+	let:dirty
+	let:valid
+	{...$$restProps}
+>
+	<slot message="{message}" dirty="{dirty}" valid="{valid}">
+		{#if dirty && !valid}
+			<div data-testid="{name}-error" class="flex items-center">
+				<Icon classes="mr-2" icon="{faExclamationCircle}" />
+				{message}
+			</div>
+		{:else}&nbsp;{/if}
+	</slot>
+</InputControl>
